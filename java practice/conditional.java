b5dfed88c4e7281eb61e05a6f191a1d3fc31274a
aryan1403 <aaryan14032006@email.com>
2024-06-24 09:29:05 +0530

    basics of java conditional, loops, scanner

---
+++ java practice/conditional.java
@@ -0,0 +1,16 @@
+public class conditional {
+    public static void main(String[] args) {
+        //comparision --> 2 things to compare
+        int a = 6;
+        int b = 6;
+
+        // 1. find the bigger
+        if(a > b) {
+            System.out.println("a is greater than b");
+        } else if(a < b) {
+            System.out.println("b is greater than a");
+        } else {
+            System.out.println("both are equal");
+        }
+    }
+}
---
+++ java practice/variables.java
@@ -0,0 +1,21 @@
+/**
+ * variables
+ */
+public class variables {
+    public static void main(String[] args) {
+        // variables
+        int a = 5;
+        float data = 5.5f;
+        double d = 34.567894;
+        boolean bool = true;
+        char ch = 'r';
+        String str = "Aaryan is a Trainer";
+
+        a = 6;
+        System.out.println("The value of a is: " + a);
+        
+        // constant --> value should not change
+        final int b = 10;
+        System.out.println(b);
+    }
+}
