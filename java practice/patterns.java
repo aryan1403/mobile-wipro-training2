85bb54d15f6ef9968b77d8e57b253fd5b4761aa1
aryan1403 <aaryan14032006@email.com>
2024-06-24 12:28:00 +0530

    patterns

--- java practice/patterns.java
+++ java practice/patterns.java
@@ -26,6 +26,52 @@ public class patterns {
          * *
          * 
          */
+
+        // TODO: print the triangle
+        /*
+              *
+            * * *
+          * * * * *
+        * * * * * * *
+        */
+        /* int n = 10;
+        for (int i = 1; i <= n; i++) {
+            if(i % 2 == 0) {
+                continue;
+            }
+            for(int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        } */
+
+        // TODO: Hollow triangle
+        /*
+                    *
+                 *     *
+               *         *
+             *             *
+            * * * * * * * * *
+         */
+        for (int i = 1; i <= 10; i++) {
+            for (int j = i; j < 10; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k < (2 * i); k++) {
+                if(k == 1 || i == 10 || k == (2 * i - 1)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+         
         
     }
 }
