b45f7f209e7acc490c635f0e64962204cf064bfb
aryan1403 <aaryan14032006@email.com>
2024-06-27 16:41:44 +0530

    test cases

--- jdbcsample/src/main/java/com/example/App.java
+++ jdbcsample/src/main/java/com/example/App.java
@@ -6,6 +6,13 @@ import java.sql.*;
 
 public class App 
 {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static boolean even(int a) {
+        return (a % 2 == 0);
+    }
     public static void main( String[] args ) throws Exception
     {
         // Create the connection
--- jdbcsample/src/test/java/com/example/AppTest.java
+++ jdbcsample/src/test/java/com/example/AppTest.java
@@ -1,20 +1,29 @@
 package com.example;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void addTest() {
+        // App app = new App();
+        int result = App.add(4, 5);
+
+        assertEquals(9, result);
+    }
+
+    @Test
+    public void evenTest() {
+        boolean result = App.even(3);
+        assertFalse(result);
+        // assertEquals(false, result);
+        assertNotEquals(true, result);
+        assertNull(null);
     }
 }
