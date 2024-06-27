0060d5ac0817cff9cd9687f87d3589c72f207001
aryan1403 <aaryan14032006@email.com>
2024-06-27 09:08:00 +0530

    multithreading

---
+++ java practice/th.java
@@ -0,0 +1,19 @@
+public class th {
+    public static void printHelloWorld() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("hello world");
+        }  
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread th = new Thread(() -> printHelloWorld());
+        Thread th2 = new Thread(() -> System.out.println(2 + 2));
+        // System.out.println(3 + 3);     
+
+        Thread.sleep(3000); // wait
+        th.start();
+        // th.join();
+        th2.start();
+        
+    }
+}
