8d7a598eeb8e60b66b5a87b1e9574af55ae5a6af
aryan1403 <aaryan14032006@email.com>
2024-06-28 09:56:50 +0530

    basic tags completed

--- html css js/index.js
+++ html css js/index.js
@@ -2,5 +2,6 @@ function clickme() {
     let a = document.getElementById("a").value;
     let b = document.getElementById("b").value;
 
-    alert(Number(a) + Number(b));
+    // alert(Number(a) + Number(b));
+    document.getElementById("result").innerText = "Result: " + (Number(a) + Number(b))
 }
